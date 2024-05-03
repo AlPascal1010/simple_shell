ceb63b9e5b4e5cba9740f49199d27748777dd8c7
George Pascal <georgepascal1010@gmail.com>
2024-05-03 23:47:54 +0100

    README.md

--- helper.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHELL_HELPER_H_
-#define SHELL_HELPER_H_
-#include <unistd.h>
-#include <sys/types.h>
-#include <errno.h>
-void UnixError(char* msg);
-pid_t Fork();
-#endif
--- io.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHELL_IO_H_
-#define SHELL_IO_H_
-#include "constants.h"
-void Print(char*);
-void PrintError(char*);
-void Flush();
-void InitLineBuffer();
-#endif
